c9c183f5342557c058905efaf46ae99c25f54c94
Tanner Koza <kozatanner@gmail.com>
2024-08-19 20:49:13 -0600

    Remove example test failing assertion

--- tests/test_coordinate_conversions.py
+++ tests/test_coordinate_conversions.py
@@ -3,7 +3,6 @@ import navtools.conversions as ntc
 
 def test_ecef_conversions():
     assert ntc.ecef2lla(1.0, 1.0, 1.0) == True
-    assert ntc.ecef2lla(1.0, 1.0, 1.0) == False
 
 
 if __name__ == "__main__":
