84b95694ca8fab1be51158e07ca542bbccfd1a99
Tanner Koza <kozatanner@gmail.com>
2025-05-14 21:57:44 -0600

    Update ecef conversion tests

--- tests/test_coordinate_conversions.py
+++ tests/test_coordinate_conversions.py
@@ -1,9 +1,18 @@
+import numpy as np
 import navtools.conversions as ntc
-from navtools.conversions.coordinates import GEODETIC
+from navtools.constants import GEODETIC_DATUMS
+
+from pytest import approx
 
 
 def test_ecef_conversions():
-    assert ntc.ecef2lla(1.0, 1.0, 1.0) == GEODETIC(lat=32.0, lon=-85.0, alt=200)
+    WGS84 = GEODETIC_DATUMS["wgs84"]
+
+    lla = np.array(ntc.ecef2geodetic(x=WGS84.r0, y=0.0, z=0.0))
+    assert lla == approx(np.zeros(3), rel=1e-9)
+
+    ecef = np.array(ntc.geodetic2ecef(lat=90.0, lon=0.0, alt=0.0, deg=True))
+    assert ecef == approx(np.array([0.0, 0.0, WGS84.rp]), abs=1e-9)
 
 
 if __name__ == "__main__":
