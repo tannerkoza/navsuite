5441751d6b43676922f67194cdd4769ae7bff337
Tanner Koza <kozatanner@gmail.com>
2025-07-07 16:31:09 -0600

    Remove numba njit from ecef2enu DCM rotation

--- src/navtools/src/navtools/conversions/coordinates.py
+++ src/navtools/src/navtools/conversions/coordinates.py
@@ -63,12 +63,13 @@ def ecef2geodetic(
     lon = np.arctan2(y, x)
     alt = (beta - datum.r0 * T) * np.cos(lat) + (
         z - np.sign(z) * datum.r0 * np.sqrt(1 - datum.eccentricity**2)
-    ) * np.sin(lat)  # Eq. C.38
+    ) * np.sin(
+        lat
+    )  # Eq. C.38
 
     return GEODETIC(lat=lat, lon=lon, alt=alt)
 
 
-@nb.njit(cache=True, fastmath=True)
 def C_ecef2enu(
     x: float | np.ndarray,
     y: float | np.ndarray,
