84ed940d8db1b0b0b28b7c1c91f67ae2e255ff9c
Tanner Koza <48143385+tannerkoza@users.noreply.github.com>
2025-07-25 11:12:49 -0600

    Add navplot (#15)
    * Add navplot package
    
    * Update python version and dependencies
    
    * Add outlier emitter rejection (#14)
    
    * Ensure 2d array for cartesian series
    
    Handles single position case.
    
    * Move GeodeticDatum to geodesy module
    
    * Fix emitter single time case and add ability to remove outliers
    
    * Apply pre-commit changes
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Add plot_satellites
    
    * Apply pre-commit changes
    
    * Fix _get_emitter_orbit_type
    
    * Fix C_enu2ecef
    
    * Update python version and dependencies
    
    * Update python version and dependencies
    
    * Format navplot pyproject.toml
    
    * Update navsuite dependencies
    
    * Change build src for wheels
    
    * Update navplot api
    
    * Format
    
    * Apply pre-commit changes
    
    * Apply pre-commit changes
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/navplot/src/navplot/__init__.py
@@ -0,0 +1,2 @@
+from .geo import *
+from .satellites import *
--- src/navtools/src/navtools/utils.py
+++ src/navtools/src/navtools/utils.py
@@ -1,3 +1,5 @@
+import itertools
+
 import numpy as np
 from numpy.typing import NDArray
 
@@ -12,3 +14,15 @@ def to_cartesian_series(array: NDArray) -> NDArray:
             array = array.transpose()
 
     return array
+
+
+def ragged_to_array(ragged: list, pad_value: any = np.nan) -> NDArray:
+    # pad missing elements after tranposing list
+    transposed = list(itertools.zip_longest(*ragged, fillvalue=pad_value))
+    array = np.array(transposed).T  # transpose to original orientation
+
+    return array
+
+
+def find_axis(arr: NDArray, axis_length: int) -> int | None:
+    return next((i for i, dim in enumerate(arr.shape) if dim == axis_length), None)
